eb280fa9205962bd09b00e552dd8608309f93b75
Murad Salameh <murad@murads-mbp-3554.myfiosgateway.com>
2022-03-24 17:21:24 -0400

    trying to get posts on homepage

--- src/components/home.js
+++ src/components/home.js
@@ -18,32 +18,36 @@ function Home() {
 
      const[posts, setPosts] = useState([]);
      useEffect(() => {
-        fetch('api/posts')
+        Axios.get("http://localhost:3001/api/posts/")
         .then((res) => res.json())
         .then((json) => setPosts(json.body));
      }, []);
 
      return (
-          <div className='Home' style={{
+               <div className='Home' style={{
                display: 'flex',
                justifyContent: 'center',
                alignItems: 'center',
                height: '90vh'
-             }}>
-               <h1>Welcome!</h1>
-               <Link to={"/login"}><button>Logout!</button></Link>
-               <div className='postForm'>
-                    <input type='text' placeholder='Something on your mind?' name='postContent' onChange ={(e) => {setBody(e.target.value);}}/>
-
-               <button onClick={post}>Post</button>
-               </div>
-               <div className='posts'>
-               <ul>
-                {posts.map((t) => (
-                <li>{t.body}</li>
-                ))}
-               </ul>
-               </div>
+               }}>
+                    <h1>Welcome!</h1>
+                    <Link to={"/login"}><button>Logout!</button></Link>
+                    <div className='postForm'>
+                        <input type='text' placeholder='Something on your mind?' name='postContent' onChange ={(e) => {setBody(e.target.value);}}/>
+                        <button onClick={post}>Post</button>
+                    </div>
+                    <div className='posts' style={{
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    height: '90vh'
+                    }}>
+                        <ul>
+                            {posts.map((t) => (
+                            <li>{t.posts}</li>
+                            ))}
+                        </ul>
+                    </div>
           </div>
      )
 }
