3417ad4b398d2f893b740662e19510356f34e516
Murad Salameh <murad@macbook-pro.myfiosgateway.com>
2022-04-08 17:03:37 -0400

    adding more user information to profile page. Still needs work

--- server/routes/user-routes.js
+++ server/routes/user-routes.js
@@ -1,15 +1,17 @@
 //This file just includes the routes for each of the user endpoints
 
-const {createUser,createProfile,login,getUserByUsername,getUsers,updateUser,deleteUser} = require('../controllers/user-controllers');
+const {createUser,createProfile,login, getUserByUserID, getProfileInfo, getUsers,updateUser,deleteUser} = require('../controllers/user-controllers');
 const router = require('express').Router();
-const {checkToken} = require('../auth/token-validation');
+//const {checkToken} = require('../auth/token-validation');
 
 router.post('/signup', createUser); //Signup Public
 router.post('/profile', createProfile);
-router.get('/:suid', getUserByUsername); //Public
+router.get('/:suid', getUserByUserID);
+//router.get('/:username', getUserByUsername); //Search Bar Route
+router.get('/:username', getProfileInfo);
 router.get('/', getUsers); //Public
-router.patch('/', checkToken, updateUser); //Protected
-router.delete('/:suid',checkToken, deleteUser); //Protected
+router.patch('/', updateUser); //Protected
+router.delete('/:suid', deleteUser); //Protected
 router.post('/login',login); //Public
 
 module.exports = router;
